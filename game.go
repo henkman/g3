a3cd6250bf3a55ab1b9bb4a11f948282426ad2ec
henkman <henkman@github.com>
2016-11-24 23:35:26 +0100

    basic jump dude

--- game.go
+++ game.go
@@ -2,25 +2,90 @@ package main
 
 import "github.com/veandco/go-sdl2/sdl"
 
+type Player struct {
+	Position      Vec
+	Velocity      Vec
+	Height        int32
+	Width         int32
+	CanDoubleJump bool
+}
+
+func (p *Player) Render(render *sdl.Renderer) {
+	render.SetDrawColor(0x00, 0xFF, 0x00, 0xFF)
+	render.FillRect(&sdl.Rect{int32(p.Position.X), int32(p.Position.Y), p.Width, p.Height})
+}
+
 type Game struct {
+	Player Player
 }
 
 func (g *Game) Init(gd GameData) {
-
+	w, h, _ := gd.Render.GetRendererOutputSize()
+	p := &g.Player
+	p.Width = 32
+	p.Height = 32 * 2
+	p.Position.X = float32(w) / 2
+	p.Position.Y = float32(h) / 2
 }
 
 func (g *Game) Run(gd GameData) Scene {
 	for {
+		var kp struct {
+			JumpUp bool
+		}
 		for e := sdl.PollEvent(); e != nil; e = sdl.PollEvent() {
-			if _, ok := e.(*sdl.QuitEvent); ok {
+			switch t := e.(type) {
+			case *sdl.QuitEvent:
 				return nil
-			} else if kd, ok := e.(*sdl.KeyUpEvent); ok && kd.Keysym.Scancode == sdl.SCANCODE_ESCAPE {
-				return nil
+			case *sdl.KeyUpEvent:
+				switch t.Keysym.Scancode {
+				case sdl.SCANCODE_ESCAPE:
+					return nil
+				case sdl.SCANCODE_SPACE:
+					kp.JumpUp = true
+				}
 			}
 		}
+		{
+			const GRAVITY = 1
+			w, h, _ := gd.Render.GetRendererOutputSize()
+			p := &g.Player
+			floored := func() bool {
+				return p.Position.Y >= float32(h-int(p.Height))
+			}
 
+			kb := sdl.GetKeyboardState()
+			if kb[sdl.SCANCODE_LEFT] != 0 {
+				p.Velocity.X += -5
+			} else if kb[sdl.SCANCODE_RIGHT] != 0 {
+				p.Velocity.X += 5
+			}
+			f := floored()
+			if (f || p.CanDoubleJump) && kp.JumpUp {
+				if !f {
+					p.CanDoubleJump = false
+				}
+				p.Velocity.Y -= 20
+			}
+			p.Velocity.Y += GRAVITY
+			p.Velocity.X = Clamp(p.Velocity.X, -5, 5)
+			p.Velocity.Y = Clamp(p.Velocity.Y, -30, 20)
+			p.Position = p.Position.Add(p.Velocity)
+
+			p.Position.X = Clamp(p.Position.X, 0, float32(w-int(p.Width)))
+			p.Position.Y = Clamp(p.Position.Y, 0, float32(h-int(p.Height)))
+
+			if floored() {
+				p.CanDoubleJump = true
+				p.Velocity.X = 0
+				p.Velocity.Y = 0
+			}
+		}
 		gd.Render.SetDrawColor(0x00, 0x00, 0x00, 0xFF)
 		gd.Render.Clear()
+		{
+			g.Player.Render(gd.Render)
+		}
 		gd.Render.Present()
 	}
 }
